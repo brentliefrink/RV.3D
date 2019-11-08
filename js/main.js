381b7472c148fff737920dca1dedd3d87ae259a2
brentliefrink <49945030+brentliefrink@users.noreply.github.com>
2019-11-08 14:55:30 +0100

    navbar effect

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@
       $('nav').removeClass('black');
     }
   })
+
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    if (document.documentElement.scrollTop > 900) {
+      document.getElementById("project-nav").className = "project active";
+    } else {
+      document.getElementById("project-nav").className = "project non-active";
+    }
+    // if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 900) {
+    //   document.getElementById("offer-nav").className = "offer active";
+    // } else {
+    //   document.getElementById("offer-nav").className = "offer non-active";
+    // }
+    // if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 1050) {
+    //   document.getElementById("contact-nav").className = "contact active";
+    // } else {
+    //   document.getElementById("contact-nav").className = "contact non-active";
+    // }
+  }
